6b7d65ab800d1786c1ac5c774de6fc45026ae93b
Dhruv69420 <dhruvmuthria@gmail.com>
2022-03-09 00:29:31 +0530

    Updates

---
+++ precache-manifest.96e766c370219d4a512d1fb02ed1504c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74c440d2754ba36a089a2b1e1a938a59",
+    "url": "/weatheria/index.html"
+  },
+  {
+    "revision": "abd44e31c171661302f3",
+    "url": "/weatheria/static/css/main.41ec3a49.chunk.css"
+  },
+  {
+    "revision": "44d91217a2106cc453b8",
+    "url": "/weatheria/static/js/2.6fc8ad99.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/weatheria/static/js/2.6fc8ad99.chunk.js.LICENSE"
+  },
+  {
+    "revision": "abd44e31c171661302f3",
+    "url": "/weatheria/static/js/main.98a76479.chunk.js"
+  },
+  {
+    "revision": "5bf9f0f0758b7594461a",
+    "url": "/weatheria/static/js/runtime-main.fdd5a6d0.js"
+  }
+]);
